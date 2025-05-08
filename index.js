62f21ab007d63e3d8764413a77f7afb971b644f0
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-05-08 11:44:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
+
+}
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+  
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbw3OYjHoEg7HOfiVjF3Sk4qqjf9YvrYZ-J_8DfxetqakBcE-rBs3amuRH_VfYTJYRHpPQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
